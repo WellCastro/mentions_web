e9c404900b45132d374d1cb8f588a18b4fe3a981
WellCastro <wcesarc@gmail.com>
2017-07-16 10:35:34 -0300

    settings and url

--- mentions_web/urls.py
+++ mentions_web/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
+
+from app.views import Index
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-]
+    url(r'^$', Index.as_view(), name='index'),
+
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
